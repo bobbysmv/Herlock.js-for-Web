9f3ab1238e3dabc6c6dd3a35ce4aed423e917cd4
Yoshinari Tanaka <bobby@sonicmoov.com>
2015-11-09 21:48:35 +0900

    Update main.js

--- test/sample/main.js
+++ test/sample/main.js
@@ -1,5 +1,4 @@
 new Script( "./lib/require.min.js" ).onload = function() {
-    require.config({ baseUrl: 'test/sample' });
     require( [ "src/Application" ], function( Application ){
         new Application().start();
     } );
